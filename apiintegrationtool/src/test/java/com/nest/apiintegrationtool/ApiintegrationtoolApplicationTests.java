49c69e84dd5ed12e1a176174c0ba31fb092557d0
krishnanunni <krishnanunni.j@nestgroup.net>
2025-09-18 15:40:49 +0530

    initial commit

---
+++ apiintegrationtool/src/test/java/com/nest/apiintegrationtool/ApiintegrationtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.apiintegrationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiintegrationtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
